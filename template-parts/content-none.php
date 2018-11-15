b4ab7d4de1cb1fb3287fef6593289db343a7d699
Tyler Rilling <tyler@underlost.net>
2018-11-15 08:18:38 -0800

    updates

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -18,34 +18,13 @@
 
 		<?php
 		if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
-
-			<p><?php
-				printf(
-					wp_kses(
-						/* translators: 1: link to WP admin new post page. */
-						__( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'seattle' ),
-						array(
-							'a' => array(
-								'href' => array(),
-							),
-						)
-					),
-					esc_url( admin_url( 'post-new.php' ) )
-				);
-			?></p>
-
 		<?php elseif ( is_search() ) : ?>
-
 			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'seattle' ); ?></p>
 			<?php
 				//get_search_form();
-
 		else : ?>
-
 			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'seattle' ); ?></p>
-			<?php
-				get_search_form();
-
+			<?php get_search_form();
 		endif; ?>
 	</div><!-- .page-content -->
 </section><!-- .no-results -->
