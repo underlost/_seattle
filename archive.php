f182ec493558ef2a4983be3e747c503e25d284d4
Tyler Rilling <tyler@underlost.net>
2018-12-22 12:13:39 -0600

    fixed div from closing on archives

--- archive.php
+++ archive.php
@@ -11,9 +11,6 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-
-		<?php
-		if ( have_posts() ) : ?>
 			<header class="page-header px-4 py-2 mb-2">
 				<?php
 					the_archive_title( '<h1 class="page-title">', '</h1>' );
@@ -21,21 +18,19 @@ get_header(); ?>
 				?>
 			</header><!-- .page-header -->
       <div class="row grid">
-      <div class="grid-sizer col-md-1 col-sm-6"></div>
-
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-				get_template_part( 'template-parts/content-grid' );
-			endwhile;
-			the_posts_navigation();
-		else :
-			get_template_part( 'template-parts/content', 'none' );
-		endif; ?>
-
-    </div>
+      	<div class="grid-sizer col-md-1 col-sm-6"></div>
+				<?php
+				if ( have_posts() ) :
+					/* Start the Loop */
+					while ( have_posts() ) : the_post();
+						get_template_part( 'template-parts/content-grid' );
+					endwhile;
+					the_posts_navigation();
+				else :
+					get_template_part( 'template-parts/content', 'none' );
+				endif; ?>
+    	</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php
-get_footer();
+<?php get_footer();
