4e72482e14d61728045631cb2b8aed7ac2ea660c
Tyler Rilling <tyler@underlost.net>
2021-09-28 00:30:09 -0700

    Fixed page templates

--- page-templates/curated-collection.php
+++ page-templates/curated-collection.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template Name: Curated Collection
  * Template Post Type: page
@@ -9,37 +10,38 @@
  *
  * @package Seattle
  */
+
+$curated_posts = get_field('posts', get_the_ID());
+//print_r($curated_posts);
 $args = array(
   'posts_per_page' => 100,
   'post_status' => 'publish',
   'ignore_sticky_posts' => 1,
-  'post__in' => get_option('sticky_posts'),
+  'post__in' => $curated_posts,
 );
 $the_query = new WP_Query($args);
-
 get_header();
 ?>
 
-	<div id="primary" class="content-area container px-0">
-		<main id="main" class="site-main">
-			<header class="sr-only">
-				<h1 class="page-title sr-only"><?php single_post_title(); ?></h1>
-			</header>
-	    <div class="row grid">
-	    <div class="grid-sizer col-md-1 col-6"></div>
-			<?php if ($the_query->have_posts()) {
-     /* Start the Loop */
-     while ($the_query->have_posts()) {
-       $the_query->the_post();
-       get_template_part('template-parts/content-grid-item');
-     }
-     wp_reset_postdata();
-   } else {
-     get_template_part('template-parts/content', 'none');
-   } ?>
-	    </div>
-		</main><!-- #main -->
-	</div><!-- #primary -->
+<div id="primary" class="content-area container px-0">
+  <main id="main" class="site-main">
+    <header class="sr-only">
+      <h1 class="page-title sr-only"><?php single_post_title(); ?></h1>
+    </header>
+    <div class="row grid">
+      <div class="grid-sizer col-md-1 col-6"></div>
+      <?php if ($the_query->have_posts()) {
+        /* Start the Loop */
+        while ($the_query->have_posts()) {
+          $the_query->the_post();
+          _seattle_grid_item(get_the_ID(), $is_fixed_width = false);
+        }
+        wp_reset_postdata();
+      } else {
+        get_template_part('template-parts/content', 'none');
+      } ?>
+    </div>
+  </main><!-- #main -->
+</div><!-- #primary -->
 
 <?php get_footer();
-
